112714a8d148773e0e1d0522e0f540392c784dd1
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-10-17 09:47:57 -0500

    seccion server and checks

---
+++ src/presentation/server.ts
@@ -0,0 +1,20 @@
+import { CheckService } from "../domain/use-cases/checks/check-service"
+import { CronService } from "./cron/cron-service"
+
+
+export class Server {
+    public static start() {
+        console.log('Server started ...')
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService(
+                    () => console.log('success'),
+                    (error) => console.log(error)
+                ).execute('https://google.com')
+            }
+        )
+
+
+    }
+}
